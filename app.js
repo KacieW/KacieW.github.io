7698ab1bfaeb8f64e1301a4c604a0711ec9e9409
KacieW <kaciewang428@gmail.com>
2017-09-25 18:27:50 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -9,4 +9,26 @@ $(document).ready(function() {
       $(this).removeClass('animated bounceIn');
     });
   });
+  var $animation_elements = $('.animation-element');
+  var $window = $(window);
+
+  function checkInView() {
+    var winHeight = $window.height();//static
+    var winTopPosition = $window.scrollTop();//changing
+    var winBottomPosition = (winTopPosition+winHeight);
+    $.each($animation_elements, function(){
+      var $element = $(this);
+      var eleHeight = $element.outerHeight();
+      var eleTopPosition = $element.offset().top;
+      var eleBottomPosition = (eleHeight+eleTopPosition);
+      if((eleBottomPosition>=winTopPosition)&&(eleTopPosition<=winBottomPosition)){
+        $element.addClass('in-view');
+      }else{
+        $element.removeClass('in-view');
+      }
+    });
+  }
+
+  $window.on('scroll resize', checkInView);
+  $window.trigger('scroll');
 });
